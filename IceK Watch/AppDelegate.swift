2bd262a470e4681860917116a77225fd883bcd85
Hamza JGUERIM <hamzajg@gmail.com>
2018-05-09 22:48:35 +0100

    Work On Ads Integration..

--- IceK Watch/AppDelegate.swift
+++ IceK Watch/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GADMobileAds.configure(withApplicationID: "ca-app-pub-9443733078787192~3059832945")
+        GADMobileAds.configure(withApplicationID: "ca-app-pub-4599577559313460~3544754968")
         return true
     }
 
